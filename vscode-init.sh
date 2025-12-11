fdac9b05ead6a2e630a268072bde26e1a535f774
jclo00 <jean-christophe.logel@eleve.ensai.fr>
2025-12-11 22:08:50 +0100

    Update vscode-init.sh

--- vscode-init.sh
+++ vscode-init.sh
@@ -1,19 +1,6 @@
 #!/usr/bin/bash
 set -e
 
-EXT_DIR="${HOME}/.local/share/code-server/extensions"
-TARGET="streetsidesoftware.code-spell-checker"
-
-mkdir -p "${EXT_DIR}"
-
-(
-    for i in $(seq 1 20); do
-        sleep 2
-        rm -rf ${EXT_DIR}/${TARGET}* 2>/dev/null || true
-    done
-) &
-
-
 USER_SETTINGS="${HOME}/.local/share/code-server/User/settings.json"
 mkdir -p "$(dirname "$USER_SETTINGS")"
 
@@ -23,8 +10,11 @@ fi
 
 tmpfile=$(mktemp)
 jq '. + { "workbench.colorTheme": "Default Dark Modern",
-           "extensions.ignoreRecommendations": true }' \
-   "$USER_SETTINGS" > "$tmpfile"
+           "extensions.ignoreRecommendations": true,
+           "extensions.disabled": [
+            "streetsidesoftware.code-spell-checker"
+        ]
+    }' "$USER_SETTINGS" > "$tmpfile"
 
 mv "$tmpfile" "$USER_SETTINGS"
 
